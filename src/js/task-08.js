9209c5d563f8b267644097518d2c176365acb955
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-04-19 00:42:36 +0200

    task_08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    
+         if (mail || password === '') {
+        alert('Вы не заполнили все поля');
+    }
+    
+
+    const formData = new FormData(event.currentTarget);
+
+    formData.forEach((value, name) => {
+        console.log('onFormSubmit -> value', value);
+        console.log('onFormSubmit -> name', name);
+
+   
+
+        form.reset();
+        
+    })
+};
