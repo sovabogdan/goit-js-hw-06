3d25491028007b9794c33fb9ed9664f5b2f07af3
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-04-18 21:04:15 +0200

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -3,7 +3,13 @@ const refs = {
     nameLabel: document.querySelector('#name-output')
 }
 function onInputChange(event) {
-    refs.nameLabel.textContent = event.currentTarget.value;
+    if (event.currentTarget.value !== '') {
+        refs.nameLabel.textContent = event.currentTarget.value;
+    }
+    else {
+        refs.nameLabel.textContent = 'Anonymous'
+    }
+    
 };
 refs.input.addEventListener('input', onInputChange);
 console.log(refs.input.currentTarget);
