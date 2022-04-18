4a90f92011ec6e8503372b9a5a1f97b26285afd4
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-04-19 01:01:41 +0200

    task_09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const textColorEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+  document.querySelector('body').style.backgroundColor = getRandomHexColor();
+  textColorEl.textContent = getRandomHexColor();
+}
