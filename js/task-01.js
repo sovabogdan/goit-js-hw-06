6c0e6046091e6f0cf8f0aeeb76b2c54c044abc9b
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-04-22 21:10:39 +0200

    debag_task_01

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,10 @@
-const body = document.body;
-const list = body.firstElementChild.nextElementSibling;
-const categories = list.children;
-console.log("Number of categories: " + categories.length);
 
-const firstCategory = categories[0];
-console.log('Category: ' + firstCategory.firstElementChild.textContent);
-const firstCategoryItems = firstCategory.querySelectorAll("li");
-console.log('Elements: '+ firstCategoryItems.length);
+const categories = document.querySelector('#categories').children;
+console.log(`Number of categories: ${categories.length}`);
 
-const secondCategory = categories[1];
-console.log('Category: ' + secondCategory.firstElementChild.textContent);
-const secondCategoryItems = secondCategory.querySelectorAll("li");
-console.log('Elements: ' + secondCategoryItems.length);
+const categoryItems = document.querySelectorAll('.item')
+    .forEach((category) => {
+        console.log('Category: ' + category.querySelector('h2').textContent),
+            console.log('Elements: ' + category.querySelectorAll('li').length)
+    });
 
-const thirdCategory = categories[2];
-console.log('Category: ' + thirdCategory.firstElementChild.textContent);
-const thirdCategoryItems = thirdCategory.querySelectorAll("li");
-console.log('Elements: '+ thirdCategoryItems.length);
