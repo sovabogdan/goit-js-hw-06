44fae73ccb11aab8bf697d129039bd0a048e212f
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-04-23 23:07:34 +0200

    debug__task__01

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,20 @@
 
-const categories = document.querySelector('#categories').children;
+/* const categories = document.querySelector('#categories').children;
 console.log(`Number of categories: ${categories.length}`);
 
 const categoryItems = document.querySelectorAll('.item')
-    .forEach((category) => {
-        console.log('Category: ' + category.querySelector('h2').textContent),
-            console.log('Elements: ' + category.querySelectorAll('li').length)
-    });
+    categories.forEach((category) => {
+        console.log('Category: ' + category.fistElementChild.textContent)
+            console.log('Elements: ' + category.lastElementChild.children.length);
+        
+}); */
+
+const categories = document.querySelectorAll('.item')
+console.log('Number of categories:', categories.length);
+categories.forEach(function (itemName) {
+    const categoriesRef = itemName.firstElementChild;
+    const elementsLengthRef = itemName.lastElementChild;
+    console.log('Category :', categoriesRef.textContent)
+    console.log('Elelments :', elementsLengthRef.children.length)
+})
 
