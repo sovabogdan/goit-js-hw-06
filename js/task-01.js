181c1a369b9eea51d0387e882f1c61a24163a3d2
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-04-23 23:08:46 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,3 @@
-
-/* const categories = document.querySelector('#categories').children;
-console.log(`Number of categories: ${categories.length}`);
-
-const categoryItems = document.querySelectorAll('.item')
-    categories.forEach((category) => {
-        console.log('Category: ' + category.fistElementChild.textContent)
-            console.log('Elements: ' + category.lastElementChild.children.length);
-        
-}); */
-
 const categories = document.querySelectorAll('.item')
 console.log('Number of categories:', categories.length);
 categories.forEach(function (itemName) {
